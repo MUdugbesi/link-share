530a04a9d23c50c13bdeda5ed8137acf752ccbbd
Marvelous <marvelous.udugbesi@icloud.com>
2024-07-24 23:32:59 +0100

    refactored types props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${instrument_Sans.className} flex min-h-screen flex-col min-w-[1440px] mx-auto`}>
-        {children}
+        
+          {children}
+        
       </body>
     </html>
   );
