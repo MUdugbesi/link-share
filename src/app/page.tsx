0f132d5fda0f9f4767aa2cb6fab02b78118ee476
Marvelous <marvelous.udugbesi@icloud.com>
2024-07-25 11:39:09 +0100

    refactored types props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 "use client"
 
-import { Provider } from "react-redux";
-import { store } from "@/store";
-import NavBar from "@/components/layouts/NavBar";
-import HomePage from "./home/page";
 import Login from "./auth/login/page";
 
 export default function Home() {
